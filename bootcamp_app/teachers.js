0cc37c6354cb366217b8301cc5fb39d03af46567
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-02-11 04:08:58 -0500

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,29 +1,49 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: 'gitt',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "gitt",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  });
 });
 
 // pool.query(`
-// SELECT cohorts.name as cohort, teachers.name as teacher 
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+// pool.query(`
+// SELECT cohorts.name as cohort, teachers.name as teacher
 // FROM students
 // JOIN cohorts ON cohorts_id = cohort.id
 // JOIN assistance_requests ON teacher_id = teachers.id
@@ -36,5 +56,3 @@ ORDER BY teacher;
 //     console.log(`${row.cohort}:${row.teacher} `);
 //   })
 // }).catch(err => console.error('query error', err.stack));
-
-
